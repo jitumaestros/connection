5dbc38ea25e75959e08bb3ad38d7aa48113208fa
jitu <60170942+jitumaestros@users.noreply.github.com>
2020-01-25 17:54:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
-$conn=mysqli_connect("dashdb-txn-sbox-yp-lon02-02.services.eu-gb.bluemix.net","lwt73682","9cf59fqf@sll05bc","BLUDB");
-if(!$conn)
-{
-	echo "database is not connect";
-}else
-{
-	echo "database connected";
+$database = 'BLUDB';
+$user = 'lwt73682';
+$password = '9cf59fqf@sll05bc';
+$hostname = 'dashdb-txn-sbox-yp-lon02-02.services.eu-gb.bluemix.net';
+$port = 50000;
+
+$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=$database;" .
+  "HOSTNAME=$hostname;PORT=$port;PROTOCOL=TCPIP;UID=$user;PWD=$password;";
+$conn = db2_connect($conn_string, '', '');
+
+if ($conn) {
+    echo "Connection succeeded.";
+    db2_close($conn);
+}
+else {
+    echo "Connection failed.";
 }
 ?>
